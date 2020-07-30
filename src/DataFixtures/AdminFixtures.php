ca3c8e8a564ab8fa022093dac7a04c033b8ef4d9
Matthieu Mota <matthieumota@gmail.com>
2020-07-30 16:36:08 +0200

    Impersonate

--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Admin;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AdminFixtures extends Fixture
@@ -18,11 +19,21 @@ class AdminFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr_FR');
+
         $admin = new Admin();
         $admin->setEmail('matthieumota@gmail.com');
         $admin->setPassword($this->encoder->encodePassword($admin, '123'));
         $admin->setRoles(['ROLE_ADMIN']);
         $manager->persist($admin);
+
+        for ($i = 1; $i <= 3; ++$i) {
+            $user = new Admin();
+            $user->setEmail($faker->email);
+            $user->setPassword($this->encoder->encodePassword($user, '123'));
+            $user->setRoles(['ROLE_ADMIN']);
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
