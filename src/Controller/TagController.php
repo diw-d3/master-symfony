ca0e76a8e969affaede0759b8d39d7b364afbb08
Matthieu Mota <matthieumota@gmail.com>
2020-07-30 12:06:47 +0200

    Use serializer

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class TagController extends AbstractController
 {
@@ -18,5 +21,28 @@ class TagController extends AbstractController
 
         return $this->json($tags, 200, [], ['groups' => 'api']);
         // return new Response(json_encode($tags));
+    }
+
+    /**
+     * @Route("/tags.{_format}", name="tag_xml")
+     */
+    public function export(
+        $_format,
+        SerializerInterface $serializer,
+        ProductRepository $repository
+    ) {
+        $products = $repository->findAll();
+
+        $content = $serializer->serialize(
+            $products,
+            $_format,
+            ['groups' => 'api']
+        );
+
+        $response = new Response($content);
+        // $response->headers->set('Content-Disposition', 'inline');
+        $response->headers->set('Content-Disposition', 'attachment; filename="export.'.$_format.'";');
+
+        return $response;
     }
 }
--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -6,6 +6,7 @@ use App\Repository\ProductRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -19,6 +20,8 @@ class Product
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
+     *
+     * @Groups({"api"})
      */
     private $id;
 
@@ -26,6 +29,8 @@ class Product
      * @ORM\Column(type="string", length=255)
      *
      * @Assert\NotBlank
+     *
+     * @Groups({"api"})
      */
     private $name;
 
@@ -33,6 +38,8 @@ class Product
      * @ORM\Column(type="text")
      *
      * @Assert\NotBlank
+     *
+     * @Groups({"api"})
      */
     private $description;
 
@@ -45,6 +52,8 @@ class Product
      *   max=10000,
      *   notInRangeMessage="This value should be between {{ min }} and 100"
      * )
+     *
+     * @Groups({"api"})
      */
     private $price;
 
